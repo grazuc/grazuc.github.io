2e28fa4956d4ae57d1d6b69d8746cede65765ef4
grazuc <71348911+grazuc@users.noreply.github.com>
2024-09-23 18:52:18 -0300

    cambiando cosas

--- fragmento.js
+++ fragmento.js
@@ -1,30 +1,9 @@
-class componenteTeamRed extends HTMLElement {
+class componenteTeamRed extends HTMLElement {   
     constructor() {
         super();
-        this.attachShadow({ mode: 'open' });
-    }
-    connectedCallback() {
-        this.render();
-    }
-    render() {
-        this.shadowRoot.innerHTML = `
-            <style>
-                h1 {
-                    color: white;
-                    background-color: red;
-                    padding: 10px;
-                    text-align: center;
-                }
-                /* Puedes agregar más estilos si necesitas */
-                :host {
-                    display: block;
-                    width: 100%;
-                    background-color: white;
-                }
-            </style>
-            <h1>Hola Mundo</h1>
-        `;
+        this.innerHTML= `<p>Sharknado</p>`;
     }
 }
 
 customElements.define('9-12-18', componenteTeamRed);
+    
